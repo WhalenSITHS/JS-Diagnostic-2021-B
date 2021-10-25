3ae6b6ede18284699dab895796872798e9fef3ce
mr. whalen <michael.whalen@sitechhs.com>
2021-10-25 08:45:47 -0400

    Ready

---
+++ Venusaur.js
@@ -0,0 +1,87 @@
+const Venusaur = {
+  abilities: [
+    {
+      ability: {
+        name: "overgrow",
+        url: "https://pokeapi.co/api/v2/ability/65/",
+      },
+      is_hidden: false,
+      slot: 1,
+    },
+    {
+      ability: {
+        name: "chlorophyll",
+        url: "https://pokeapi.co/api/v2/ability/34/",
+      },
+      is_hidden: true,
+      slot: 3,
+    },
+  ],
+  stats: [
+    {
+      base_stat: 80,
+      effort: 0,
+      stat: {
+        name: "hp",
+        url: "https://pokeapi.co/api/v2/stat/1/",
+      },
+    },
+    {
+      base_stat: 82,
+      effort: 0,
+      stat: {
+        name: "attack",
+        url: "https://pokeapi.co/api/v2/stat/2/",
+      },
+    },
+    {
+      base_stat: 83,
+      effort: 0,
+      stat: {
+        name: "defense",
+        url: "https://pokeapi.co/api/v2/stat/3/",
+      },
+    },
+    {
+      base_stat: 100,
+      effort: 2,
+      stat: {
+        name: "special-attack",
+        url: "https://pokeapi.co/api/v2/stat/4/",
+      },
+    },
+    {
+      base_stat: 100,
+      effort: 1,
+      stat: {
+        name: "special-defense",
+        url: "https://pokeapi.co/api/v2/stat/5/",
+      },
+    },
+    {
+      base_stat: 80,
+      effort: 0,
+      stat: {
+        name: "speed",
+        url: "https://pokeapi.co/api/v2/stat/6/",
+      },
+    },
+  ],
+  types: [
+    {
+      slot: 1,
+      type: {
+        name: "grass",
+        url: "https://pokeapi.co/api/v2/type/12/",
+      },
+    },
+    {
+      slot: 2,
+      type: {
+        name: "poison",
+        url: "https://pokeapi.co/api/v2/type/4/",
+      },
+    },
+  ],
+  weight: 1000,
+};
---
+++ diagnostic.js
@@ -0,0 +1,9 @@
+//Q1) Identify the data type found in Venusaur.js
+
+//Q2)Write a console log that references Venusaur's base_stat "defense" value?
+
+//Q3) Write a function that iterates through Venusaur's "Types" so that you ONLY print the type names in the console?
+
+//Q4) When using Fetch, what data type is  found after resolving the Promise?
+
+//Q5) Using Async Await, write a function that logs ONLY the abilities of the pokemon found at the following URL Endpoint "https://pokeapi.co/api/v2/pokemon/steelix"
